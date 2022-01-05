5c71a2ae98e293b41eb0e795ed8e25b885ad8e57
lenubone <gsstreck@gmail.com>
2022-01-04 21:44:37 -0300

    five commit

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-use app\Http\Controllers\EventController;
+use App\http\Controllers\EventController;
 
 Route::get('/',[EventController::class, 'index']);
 
